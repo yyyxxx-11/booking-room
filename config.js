4b970a74f20a04ddc7b19f278b45d6fd9f9c846a
yyyxxx-11 <1549432161@qq.com>
2026-09-09 10:10:21 +0800

    Add booking configuration to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.BOOKING_CONFIG = {
+  SUPABASE_URL: "https://fjxpohyvnxownamoddex.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_fBunRJHSkLSWKJfIQZZakg_GRTFhtyW",
+  ROOM_NAME: "协作会议室"
+};
