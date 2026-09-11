ecf54eb1aaac37241eb3435199a532df046115c9
yyyxxx-11 <1549432161@qq.com>
2026-09-11 13:50:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.BOOKING_CONFIG = {
   SUPABASE_URL: "https://fjxpohyvnxownamoddex.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_fBunRJHSkLSWKJfIQZZakg_GRTFhtyW",
-  ROOM_NAME: "协作会议室"
+  ROOM_NAME: "Meeting Room",
+  BOOTH_NAME: "Exhibition Booth"
 };
