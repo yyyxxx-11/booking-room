73b486d4b027c2289e6ca370262bd840616e283d
yyyxxx-11 <1549432161@qq.com>
2026-09-23 09:25:35 +0800

    Update BOOKING_CONFIG by removing room names
    Removed ROOM_NAME and BOOTH_NAME from configuration.

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 window.BOOKING_CONFIG = {
   SUPABASE_URL: "https://fjxpohyvnxownamoddex.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_fBunRJHSkLSWKJfIQZZakg_GRTFhtyW",
-  ROOM_NAME: "Meeting Room",
-  BOOTH_NAME: "Exhibition Booth"
+  SUPABASE_ANON_KEY: "sb_publishable_fBunRJHSkLSWKJfIQZZakg_GRTFhtyW"
 };
